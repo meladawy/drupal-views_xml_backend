6c57ee30ded9bd3f4523e4dbf645b9b50ea5cee6
Chris Leppanen <chris.leppanen@gmail.com>
2016-02-23 11:15:06 -0800

    Add the passthrough and numeric argument handlers.

---
+++ src/Plugin/views/argument/Numeric.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\views_xml_backend\Plugin\views\argument\Numeric,
+ */
+
+namespace Drupal\views_xml_backend\Plugin\views\argument;
+
+/**
+ * Numeric XML argument handler.
+ *
+ * @ingroup views_argument_handlers
+ *
+ * @ViewsArgument("views_xml_backend_numeric")
+ */
+class Numeric extends Standard {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __toString() {
+    return $this->options['xpath_selector'] . '=' . $this->getValue();
+  }
+
+}
